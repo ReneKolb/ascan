42eb20bd985d20c2ee5babd887f63378408eaa98
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-04-24 23:10:55 +0200

    Implement back navigation.

--- src/main/java/com/hbm/devices/scan/ui/android/ScanActivity.java
+++ src/main/java/com/hbm/devices/scan/ui/android/ScanActivity.java
@@ -6,8 +6,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.app.FragmentManager;
 
-public class ScanActivity extends Activity {
+public class ScanActivity extends Activity implements FragmentManager.OnBackStackChangedListener {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -16,12 +17,33 @@ public class ScanActivity extends Activity {
 			if (savedInstanceState != null) {
 				return;
 			}
+			getFragmentManager().addOnBackStackChangedListener(this);
+			shouldDisplayHomeUp();
+
 			DeviceFragment devices = new DeviceFragment();
 			devices.setArguments(getIntent().getExtras());
 			getFragmentManager().beginTransaction()
 				.add(R.id.fragment_container, devices).commit();
 
 		}
+	}
+
+	@Override
+	public void onBackStackChanged() {
+	    shouldDisplayHomeUp();
+	}
+
+	private void shouldDisplayHomeUp(){
+	   //Enable Up button only  if there are entries in the back stack
+	   boolean canback = getFragmentManager().getBackStackEntryCount() > 0;
+	   getActionBar().setDisplayHomeAsUpEnabled(canback);
+	}
+	
+	@Override
+	public boolean onNavigateUp() {
+    	//This method is called when the up button is pressed. Just the pop back stack.
+		getFragmentManager().popBackStack();
+		return true;
 	}
 
 	@Override
