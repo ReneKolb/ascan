60388a3a45ccb9fad9d66ecad0605f34344b1e40
Stephan Gatzka <stephan.gatzka@gmail.com>
2014-04-24 23:52:38 +0200

    Correct back navigation from action bar.

--- src/main/java/com/hbm/devices/scan/ui/android/SettingsActivity.java
+++ src/main/java/com/hbm/devices/scan/ui/android/SettingsActivity.java
@@ -1,14 +1,29 @@
 package com.hbm.devices.scan.ui.android;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.view.MenuItem;
 
 public class SettingsActivity extends PreferenceActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 		getFragmentManager().beginTransaction()
 			.replace(android.R.id.content, new SettingsFragment())
 			.commit();
 	}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+		System.out.println("option");
+        switch (item.getItemId()) {
+        case android.R.id.home:
+    	    super.onBackPressed();  
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
 }
